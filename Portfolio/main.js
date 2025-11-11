26df4fe78ba1f9bc8c093b33b0af3a3f6b7071f9
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-11-10 18:56:21 -0800

    created image grid with toggling

---
+++ Portfolio/main.js
@@ -0,0 +1,18 @@
+const makeupButton = document.querySelector("#makeup_btn");
+const hairStyleButton = document.querySelector("#hair_style_btn");
+const boxMakeup = document.querySelector(".boxes-makeup");
+const boxHairStyle = document.querySelector(".boxes-hair-style");
+
+makeupButton.addEventListener("click", () => {
+  hairStyleButton.classList.remove("toggle-selected");
+  makeupButton.classList.add("toggle-selected");
+  boxMakeup.classList.add("box-selected");
+  boxHairStyle.classList.remove("box-selected");
+});
+
+hairStyleButton.addEventListener("click", () => {
+  makeupButton.classList.remove("toggle-selected");
+  hairStyleButton.classList.add("toggle-selected");
+  boxHairStyle.classList.add("box-selected");
+  boxMakeup.classList.remove("box-selected");
+});
