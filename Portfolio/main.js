8b95b2776ab7d1e28c6886417987c95ecc6cc671
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-11-10 22:18:45 -0800

    added view-more-button function

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -2,12 +2,18 @@ const makeupButton = document.querySelector("#makeup_btn");
 const hairStyleButton = document.querySelector("#hair_style_btn");
 const boxMakeup = document.querySelector(".boxes-makeup");
 const boxHairStyle = document.querySelector(".boxes-hair-style");
+const viewMoreButton = document.querySelector("#view-more-button");
+const optionalImage = document.querySelectorAll("#optional");
 
 makeupButton.addEventListener("click", () => {
   hairStyleButton.classList.remove("toggle-selected");
   makeupButton.classList.add("toggle-selected");
   boxMakeup.classList.add("box-selected");
   boxHairStyle.classList.remove("box-selected");
+  optionalImage.forEach((element) => {
+    element.classList.add("responsive-hidden");
+  });
+  viewMoreButton.textContent = "View More";
 });
 
 hairStyleButton.addEventListener("click", () => {
@@ -15,10 +21,24 @@ hairStyleButton.addEventListener("click", () => {
   hairStyleButton.classList.add("toggle-selected");
   boxHairStyle.classList.add("box-selected");
   boxMakeup.classList.remove("box-selected");
+  optionalImage.forEach((element) => {
+    element.classList.add("responsive-hidden");
+  });
+  viewMoreButton.textContent = "View More";
 });
 
-const viewMoreButton = document.querySelector("#view-more-button");
-const optionalImage = document.querySelector("#optional");
 viewMoreButton.addEventListener("click", () => {
-  optionalImage.classList.remove("responsive-hidden");
+  if (viewMoreButton.textContent === "View More") {
+    optionalImage.forEach((element) => {
+      element.classList.remove("responsive-hidden");
+    });
+    viewMoreButton.classList.add("action");
+    viewMoreButton.textContent = "View Less";
+  } else if (viewMoreButton.textContent === "View Less") {
+    optionalImage.forEach((element) => {
+      element.classList.add("responsive-hidden");
+    });
+    viewMoreButton.textContent = "View More";
+    viewMoreButton.classList.remove("action");
+  }
 });
